b92ac94be6e49b94804155ab4b077a3fee2ddae3
Stephen Yoon <yoonic@gmail.com>
2019-10-29 22:06:30 -0400

    Fixed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes, Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { OfferAdminComponent } from './components/offer-admin/offer-admin.component';
 import { OfferBookingComponent } from './components/offer-booking/offer-booking.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
 const appRoutes: Routes = [
-    { path: 'home', component: AppComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'offer', component: OfferAdminComponent },
     { path: 'booking', component: OfferBookingComponent },
     {
@@ -25,10 +28,14 @@ const appRoutes: Routes = [
     AppComponent,
     OfferAdminComponent,
     OfferBookingComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     AppRoutingModule
   ],
   providers: [],
